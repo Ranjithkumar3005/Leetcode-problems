692ed37bcf24d6f43cb804c4caf5bba733cf14b6
Ranjithkumar3005 <srk30052004@gmail.com>
2025-04-01 12:53:46 +0530

    dum

---
+++ file.py
@@ -0,0 +1,7 @@
+# with open("ran_file.txt", "a") as file:
+#     file.write("\nGood Morning")
+
+with open("ran_file.txt", "r") as file:
+    for line in file:
+        with open("new_file", "a") as file:
+            file.write(line)
